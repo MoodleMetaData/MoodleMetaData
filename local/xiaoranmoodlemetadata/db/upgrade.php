4807ae02e502b7460b595f3434edd1f6846a32e6
Xiaoran Huang <wscqkevin@gmail.com>
2016-02-01 16:48:47 -0700

    ds

--- local/xiaoranmoodlemetadata/db/upgrade.php
+++ local/xiaoranmoodlemetadata/db/upgrade.php
@@ -10,23 +10,76 @@
     	* The owner is identified by the user ID and is a foreign key reference.
     	*/
     	if ($oldversion < 2015111701) {
-        	  // Define field userid to be added to feedback_form.
-        	$table = new xmldb_table('feedback_form');
-        	$field = new xmldb_field('userid', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'description');
 
-        	// Conditionally launch add field userid.
-        	if (!$dbman->field_exists($table, $field)) {
-        	    	$dbman->add_field($table, $field);
-        	}
+		// Define table courseinfo to be created.
+		$table = new xmldb_table('courseinfo');
 
-    ...
-        // Conditionally launch add field visibility.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
+		// Adding fields to table courseinfo.
+		$table->add_field('coursename', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('coursetopic', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('coursedescription', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('courseinstructor', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('coursefaculty', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
 
-        // Feedback_ec10 savepoint reached.
-        upgrade_plugin_savepoint(true, 2015111701, 'local', 'feedback_ec10');
+		// Adding keys to table courseinfo.
+		$table->add_key('courseid', XMLDB_KEY_PRIMARY, array('courseid'));
 
-    }
+		// Conditionally launch create table for courseinfo.
+		if (!$dbman->table_exists($table)) {
+		    $dbman->create_table($table);
+		}
+
+		// Xiaoranmoodlemetadata savepoint reached.
+		upgrade_plugin_savepoint(true, 2015111701, 'local', 'xiaoranmoodlemetadata');
+	}
+    	if ($oldversion < 2015111701) {
+
+		// Define table courseassessment to be created.
+		$table = new xmldb_table('courseassessment');
+
+		// Adding fields to table courseassessment.
+		$table->add_field('assesmentname', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('assesmentweight', XMLDB_TYPE_INTEGER, '2', null, null, null, '0');
+		$table->add_field('assessmentduedate', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+		$table->add_field('assesmentid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+		// Adding keys to table courseassessment.
+		$table->add_key('courseid', XMLDB_KEY_FOREIGN, array('courseid'), 'courseinfo', array('courseid'));
+		$table->add_key('assesmentid', XMLDB_KEY_PRIMARY, array('assesmentid'));
+
+		// Conditionally launch create table for courseassessment.
+		if (!$dbman->table_exists($table)) {
+		    $dbman->create_table($table);
+		}
+
+		// Xiaoranmoodlemetadata savepoint reached.
+		upgrade_plugin_savepoint(true, 2015111701, 'local', 'xiaoranmoodlemetadata');
+    	}
+    	if ($oldversion < 2015111701) {
+
+		// Define table coursereadings to be created.
+		$table = new xmldb_table('coursereadings');
+
+		// Adding fields to table coursereadings.
+		$table->add_field('readingurl', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('readingname', XMLDB_TYPE_TEXT, null, null, null, null, null);
+		$table->add_field('readingid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+		$table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+		// Adding keys to table coursereadings.
+		$table->add_key('readingid', XMLDB_KEY_PRIMARY, array('readingid'));
+		$table->add_key('courseid', XMLDB_KEY_FOREIGN, array('courseid'), 'courseinfo', array('courseid'));
+
+		// Conditionally launch create table for coursereadings.
+		if (!$dbman->table_exists($table)) {
+		    $dbman->create_table($table);
+		}
+
+		// Xiaoranmoodlemetadata savepoint reached.
+		upgrade_plugin_savepoint(true, 2015111701, 'local', 'xiaoranmoodlemetadata');
+    	}
+
+
 ?>
