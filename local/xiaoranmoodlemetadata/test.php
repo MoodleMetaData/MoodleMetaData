da2c7b79293c95000da260ebe5bdb7c09d78882e
ayunita <yunita@ualberta.ca>
2016-02-02 23:00:59 +0000

    a

--- local/xiaoranmoodlemetadata/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-	echo "this is just a test";
-	global $DB;
-	$user = $DB->get_record_sql('Select Column_name from Information_schema.columns where Table_name like ?'table name'', array('mdl_courseinfo'));
-	echo $user;
-?>
