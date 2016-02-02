fecffb3cc872fb8df773e1c1b955a31207a38410
ayunita <yunita@ualberta.ca>
2016-02-02 14:28:42 -0700

    added demo

--- local/yunita/lang/en/local_yunita.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $string['pluginname'] = 'YUNITA Plug-in';
-?>
--- local/yunita/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $plugin->version = 2016012300;
-    $plugin->requires = 2011033006;
-    $plugin->component = 'local_yunita';
-    $plugin->maturity = MATURITY_ALPHA;
-?>
