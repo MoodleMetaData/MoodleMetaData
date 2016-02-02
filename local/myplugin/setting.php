6986dcc5fedecea32e8c26d4e8974c78bb40ea36
ayunita <yunita@ualberta.ca>
2016-02-02 21:00:47 +0000

    copied demo

--- local/myplugin/setting.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$ADMIN->add('root', new admin_category('local_myplugin', get_string('menuoption', 'local_myplugin')));
-	$ADMIN->add('local_myplugin', new admin_externalpage('myplugin', get_string('pluginname', 'local_myplugin'),
-    $CFG->wwwroot.'/local/myplugin/view.php', 'local/myplugin:add'));
-?>
