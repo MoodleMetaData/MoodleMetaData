e9e91d90679da0e11c7844f316c9283dd0581a4a
ayunita <yunita@ualberta.ca>
2016-02-04 23:37:35 +0000

    added db in form

--- local/metadata/general_form.php
+++ local/metadata/general_form.php
@@ -9,8 +9,29 @@ class general_form extends moodleform {
 		$mform = $this->_form; //Tell this object to initialize with the properties of the Moodle form.
 
 		// Form elements
-		//$mform->addElement('text', 'email', get_string('email'));
-
+		
+		// add textbox
+		$mform->addElement('text', 'email', get_string('email'));
+		
+		// display course from database
+		$courses = array();
+		$course_query = "SELECT * FROM {course}";
+		if($result=$DB->get_records_sql($course_query)){
+			foreach($result as $item){
+				$courses[] = $item->shortname;
+			}
+		}
+		$mform->addElement('select', 'type', get_string('courses', 'local_metadata'), $courses, $attributes);
+		
+		// add buttons
+		// normally you use add_action_buttons instead of this code
+		$buttonarray=array();
+		$buttonarray[] = &$mform->createElement('submit', 'submitbutton', get_string('savechanges'));
+		$buttonarray[] = &$mform->createElement('reset', 'resetbutton', get_string('revert'));
+		$buttonarray[] = &$mform->createElement('cancel');
+		$mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+		$mform->closeHeaderBefore('buttonar');
+	
 	}
 	
 	//If you need to validate your form information, you can override  the parent's validation method and write your own.	
