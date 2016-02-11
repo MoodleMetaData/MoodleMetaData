0ce495f61816bb857e84b5df9c6f1eae8d2f2a17
laingsoft <cclaing@ualberta.ca>
2016-02-11 14:52:21 -0700

    Test

--- local/metadata/assessment_form.php
+++ local/metadata/assessment_form.php
@@ -13,6 +13,9 @@ class assessment_form extends moodleform {
 
 		// Form elements
 		//$mform->addElement('text', 'email', get_string('email'));
+		$assessment_description_text = $mform->addElement('textarea', 'assessment_description', get_string('assessment__description;, 'local_metadata'), 'wrap="virtual" rows="10" cols="70"');
+		$mform->addRule('course_description', get_string('required'),'required', null, 'client');
+		$mform->setType('assessment_description',PARAM_RAW);
 
 
 		$this->add_action_buttons();
