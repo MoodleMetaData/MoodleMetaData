29f48ce72b4b112dcdc3f9b97b63804f24913a72
ayunita <yunita@ualberta.ca>
2016-02-11 20:36:12 +0000

    add fields of sessionnumber and assessment number to the courseinfo table

--- local/metadata/version.php
+++ local/metadata/version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2016020801; //yyyymmddxx
+$plugin->version = 2016020802; //yyyymmddxx
 $plugin->requires = 2011033006;
 $plugin->component = 'local_metadata';
 $plugin->maturity = MATURITY_ALPHA;
