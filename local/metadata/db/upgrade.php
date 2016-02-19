f3d3de92bdc3804735708ec0f1b5d62d3fba2f38
Xiaoran Huang <wscqkevin@gmail.com>
2016-02-19 14:05:18 -0700

    Updated database

--- local/metadata/db/upgrade.php
+++ local/metadata/db/upgrade.php
@@ -8,60 +8,7 @@
    	/**
     	* Version 2016020801 adds all the basic tables
          */
-    if ($oldversion < 2016020803) {
-
-        // Define field assessmentnumber to be added to courseinfo.
-        $table = new xmldb_table('courseinfo');
-        $field = new xmldb_field('assessmentnumber', XMLDB_TYPE_INTEGER, '20', null, null, null, null, 'courseid');
-
-        // Conditionally launch add field assessmentnumber.
-        if (!$dbman->field_exists($table, $field)) {
-                $dbman->add_field($table, $field);
-                }
-    
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020803, 'local', 'metadata');
-    }        
-
-    if ($oldversion < 2016020803) {
-
-    // Define field sessionnumber to be added to courseinfo.
-        $table = new xmldb_table('courseinfo');
-        $field = new xmldb_field('sessionnumber', XMLDB_TYPE_INTEGER, '20', null, null, null, null, 'assessmentnumber');
-
-        // Conditionally launch add field sessionnumber.
-        if (!$dbman->field_exists($table, $field)){
-            $dbman->add_field($table, $field);
-        }
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020803, 'local', 'metadata');
-    }
-
-    if ($oldversion < 2016020801) {
-
-        // Define table courseinstructors to be created.
-        $table = new xmldb_table('courseinstructors');
-
-        // Adding fields to table courseinstructors.
-        $table->add_field('name', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-        $table->add_field('instructorid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('officelocation', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('officehours', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('type', XMLDB_TYPE_TEXT, null, null, null, null, null);
-
-        // Adding keys to table courseinstructors.
-        $table->add_key('instructorid', XMLDB_KEY_PRIMARY, array('instructorid'));
-
-        // Conditionally launch create table for courseinstructors.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
-
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020801, 'local', 'metadata');
-    }
-
-    if ($oldversion < 2016020801) {
+     if ($oldversion < 2016021901) {
 
         // Define table courseinfo to be created.
         $table = new xmldb_table('courseinfo');
@@ -70,52 +17,42 @@
         $table->add_field('coursename', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('coursetopic', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('coursedescription', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('courseinstructor', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
         $table->add_field('coursefaculty', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('assessmentnumber', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
+        $table->add_field('sessionnumber', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
 
         // Adding keys to table courseinfo.
         $table->add_key('courseid', XMLDB_KEY_PRIMARY, array('courseid'));
-        $table->add_key('courseinstructor', XMLDB_KEY_FOREIGN, array('courseinstructor'), 'courseinstructors', array('instructorid'));
 
         // Conditionally launch create table for courseinfo.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
-
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020801, 'local', 'metadata');
-    }
-
-    if ($oldversion < 2016020801) {
+        
+        
 
         // Define table courseassessment to be created.
         $table = new xmldb_table('courseassessment');
 
         // Adding fields to table courseassessment.
-        $table->add_field('assesmentname', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('assesmentweight', XMLDB_TYPE_INTEGER, '2', null, null, null, '0');
+        $table->add_field('assessmentname', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('assessmentweight', XMLDB_TYPE_INTEGER, '3', null, null, null, '0');
         $table->add_field('assessmentduedate', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('assesmentid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('assessmentid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('description', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('type', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('objective', XMLDB_TYPE_TEXT, null, null, null, null, null);
 
         // Adding keys to table courseassessment.
         $table->add_key('courseid', XMLDB_KEY_FOREIGN, array('courseid'), 'courseinfo', array('courseid'));
-        $table->add_key('assesmentid', XMLDB_KEY_PRIMARY, array('assesmentid'));
+        $table->add_key('assessmentid', XMLDB_KEY_PRIMARY, array('assessmentid'));
 
         // Conditionally launch create table for courseassessment.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
 
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020801, 'local', 'metadata');
-    }
-
-    if ($oldversion < 2016020801) {
 
         // Define table coursereadings to be created.
         $table = new xmldb_table('coursereadings');
@@ -123,7 +60,7 @@
         // Adding fields to table coursereadings.
         $table->add_field('readingurl', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('readingname', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('readingid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('readingid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
 
         // Adding keys to table coursereadings.
@@ -134,19 +71,13 @@
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
-
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020801, 'local', 'metadata');
-    }
-   	
-
-    if ($oldversion < 2016020801) {
-
+        
+        
         // Define table coursesession to be created.
         $table = new xmldb_table('coursesession');
 
         // Adding fields to table coursesession.
-        $table->add_field('sessionid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('sessionid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('sessiondate', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('sessiontopic', XMLDB_TYPE_TEXT, null, null, null, null, null);
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
@@ -160,14 +91,83 @@
         // Conditionally launch create table for coursesession.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
+        }             
+    
+
+        // Define table courseinstructors to be created.
+        $table = new xmldb_table('courseinstructors');
+
+        // Adding fields to table courseinstructors.
+        $table->add_field('name', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('instructorid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('officelocation', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('officehours', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('type', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table courseinstructors.
+        $table->add_key('instructorid', XMLDB_KEY_PRIMARY, array('instructorid'));
+        $table->add_key('courseid', XMLDB_KEY_FOREIGN, array('courseid'), 'courseinfo', array('courseid'));
+
+        // Conditionally launch create table for courseinstructors.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
         }
 
-        // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020801, 'local', 'metadata');
-    }
 
-    if ($oldversion < 2016020801) {
 
+        // Define table courselbjectives to be created.
+        $table = new xmldb_table('courselbjectives');
+
+        // Adding fields to table courselbjectives.
+        $table->add_field('objectiveid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('objectivename', XMLDB_TYPE_TEXT, null, null, null, null, null);
+
+        // Adding keys to table courselbjectives.
+        $table->add_key('objectiveid', XMLDB_KEY_PRIMARY, array('objectiveid'));
+
+        // Conditionally launch create table for courselbjectives.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+        
+        
+        
+        // Define table sessionobjectives to be created.
+        $table = new xmldb_table('sessionobjectives');
+
+        // Adding fields to table sessionobjectives.
+        $table->add_field('sessionid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('objectiveid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table sessionobjectives.
+        $table->add_key('sessionid', XMLDB_KEY_FOREIGN, array('sessionid'), 'coursesession', array('sessionid'));
+        $table->add_key('objectiveid', XMLDB_KEY_FOREIGN, array('objectiveid'), 'courselobjective', array('objectiveid'));
+
+        // Conditionally launch create table for sessionobjectives.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+        
+        
+
+        // Define table assessmentobjectives to be created.
+        $table = new xmldb_table('assessmentobjectives');
+
+        // Adding fields to table assessmentobjectives.
+        $table->add_field('assessmentid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('objectiveid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table assessmentobjectives.
+        $table->add_key('assessmentid', XMLDB_KEY_FOREIGN, array('assessmentid'), 'courseassessment', array('assessmentid'));
+        $table->add_key('objectiveid', XMLDB_KEY_FOREIGN, array('objectiveid'), 'courselobjective', array('objectiveid'));
+
+        // Conditionally launch create table for assessmentobjectives.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+        
+        
         // Define table coursetag to be created.
         $table = new xmldb_table('coursetag');
 
@@ -176,15 +176,18 @@
         $table->add_field('courseid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
 
         // Adding keys to table coursetag.
-        $table->add_key('tagid', XMLDB_KEY_PRIMARY, array('tagid'));
         $table->add_key('courseid', XMLDB_KEY_FOREIGN, array('courseid'), 'courseinfo', array('courseid'));
+        $table->add_key('tagid', XMLDB_KEY_FOREIGN, array('tagid'), 'courselobjective', array('objectiveid'));
 
         // Conditionally launch create table for coursetag.
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
-        }
+        }               
+    
+                
 
         // Metadata savepoint reached.
-        upgrade_plugin_savepoint(true, 2016020801, 'local', 'metadata');
+        upgrade_plugin_savepoint(true, 2016021901, 'local', 'metadata');
     }
+
 ?>
