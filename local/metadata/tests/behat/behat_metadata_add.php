68e7ff1bd23e86dacf87dcd048e7d93d6af3ddbe
Diusrex <diusrex@gmail.com>
2016-03-10 14:18:49 -0700

    Added a generator for learning objectives and assessments

--- local/metadata/tests/behat/behat_metadata_add.php
+++ local/metadata/tests/behat/behat_metadata_add.php
@@ -7,6 +7,70 @@ use Behat\Gherkin\Node\TableNode as TableNode;
     
 class behat_metadata_add extends behat_base {
     
+    
+    /**
+     * Creates N course sessions. Nothing is filled in for them
+     *
+     * @Given /^I create the following learning objectives for course "([^"]*)":$/
+     *
+     * @param TableNode $data Each row will be a learning objective. Write out:
+     *        | objectivename | objectivetype |
+     *        | First Name | First Type |
+     * @param string $course_short The short name of the course
+     *
+     * @return Given[]
+     */
+    public function i_create_learning_objectives_for_course($course_short, TableNode $table) {
+        global $DB;
+        
+        if (!$course_id = $DB->get_field('course', 'id', array('shortname' => $course_short))) {
+            throw new Exception('The specified course with shortname "' . $course_short . '" does not exist');
+        }
+        
+        foreach ($table->getHash() as $learning_objective) {
+            $record = array();
+            $record['objectivename'] = $learning_objective['objectivename'];
+            $record['objectivetype'] = $learning_objective['objectivetype'];
+            
+            // Add to learningobjectives
+            $id = $DB->insert_record('learningobjectives', $record);
+            
+            // Link to the wanted course
+            $courseobjective = array();
+            $courseobjective['objectiveid'] = $id;
+            $courseobjective['courseid'] = $course_id;
+            $id = $DB->insert_record('courseobjectives', $courseobjective);
+        }
+        
+        return array();
+    }
+    
+    
+    /**
+     * Creates N course sessions. Nothing is filled in for them
+     *
+     * @Given /^I create the following assessments for course "([^"]*)":$/
+     *
+     * @param TableNode $data Each row will be a assessments. Should have at least the title. Everything else is optional
+     * @param string $course_short The short name of the course
+     *
+     * @return Given[]
+     */
+    public function i_create_assessments_for_course($course_short, TableNode $table) {
+        global $DB;
+        
+        if (!$course_id = $DB->get_field('course', 'id', array('shortname' => $course_short))) {
+            throw new Exception('The specified course with shortname "' . $course_short . '" does not exist');
+        }
+        
+        foreach ($table->getHash() as $assessment) {
+            $assessment['courseid'] = $course_id;
+            $DB->insert_record('courseassessment', $assessment);
+        }
+        
+        return array();
+    }
+    
     /**
      * Creates N course sessions. Nothing is filled in for them
      *
