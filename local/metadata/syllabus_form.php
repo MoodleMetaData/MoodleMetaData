4394fa6c577ae487b78961dfdfd5391e4fcb4208
xiaoran1 <wscqkevin@gmail.com>
2016-03-17 05:12:05 +0800

    syllubus interface update

--- local/metadata/syllabus_form.php
+++ local/metadata/syllabus_form.php
@@ -51,17 +51,24 @@ class syllabus_form extends moodleform {
 		global $course, $courseId;
 	}
 	
+	function GetTimeStamp($timestamp)
+	{
+		return  gmdate("Y-m-d", $timestamp);
+	}
+	
 	function do_generate($optionno){
 		global $CFG, $DB, $USER;
 		global $course;
 		$sessionnumber = 0;
 		$readingnumber = 0;
-		$instructoremail = '';
-		$officehours = '';
-		$officelocation = '';
-		$courseInstructor = '';
+		$objectivegnumber = 0;
+		$assessmentnumber = 0;
+		$instructoremail = 'To be assigned';
+		$officehours = 'To be assigned';
+		$officelocation = 'To be assigned';
+		$courseInstructor = 'To be assigned';
 		$phonenumber = 0;
-		$instructoremail = '';
+		$instructoremail = 'To be assigned';
 		$coursedescription = '';
 		
 //collecting relative data from database===============================================================================		
@@ -75,17 +82,27 @@ class syllabus_form extends moodleform {
 				$instructoremail =  $existInstructorInfo->email;
 				$phonenumber = $existInstructorInfo->phonenumber;
 			}
+			
 			$sessionnumber = $DB->count_records('coursesession', array('courseid'=>$existCourseInfo->courseid));
 			if($sessionnumber>0){
 				$coursesessions = $DB->get_records('coursesession', array('courseid'=>$existCourseInfo->courseid));			
 			}	
+			
 			$readingnumber = $DB->count_records('coursereadings', array('courseid'=>$existCourseInfo->courseid));
 			if($readingnumber>0){
 				$coursereadings = $DB->get_records('coursereadings', array('courseid'=>$existCourseInfo->courseid));
 			}
 			
-			$courseobjectives = $DB->get_records('courseobjectives', array('courseid'=>$existCourseInfo->courseid));
-			$courseassessments = $DB->get_records('courseassessment', array('courseid'=>$existCourseInfo->courseid));
+			$objectivegnumber = $DB->count_records('coursereadings', array('courseid'=>$existCourseInfo->courseid));
+			if($objectivegnumber>0){				
+				$courseobjectives = $DB->get_records('courseobjectives', array('courseid'=>$existCourseInfo->courseid));	
+			}
+			
+			$assessmentnumber = $DB->count_records('courseassessment', array('courseid'=>$existCourseInfo->courseid));
+			if($assessmentnumber>0){
+				$courseassessments = $DB->get_records('courseassessment', array('courseid'=>$existCourseInfo->courseid));
+			}
+
 		}
 		
 
@@ -102,207 +119,203 @@ class syllabus_form extends moodleform {
 		$pdf->setPrintHeader(false);
 		$pdf->setPrintFooter(false);
 		
-	// set font
-	$pdf->SetFont('times', 'B', 20);
-	// add a page
-	$pdf->AddPage();
-	$logo = '<p><img src="ualbertalogo.jpg" alt="test alt attribute" width="200" height="70" border="0" /></p>';
-	$pdf->writeHTMLCell(0, 0, '', '', $logo, 0, 1, 0, true, 'L', true);
-	$coursefullname = $course->shortname.': '.$course->fullname;
-	$pdf->Cell(0, 0, '', 0, 0, 'C');
-	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');
-	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');
-	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');
-	$pdf->Ln();
-	$pdf->Cell(0, 0, $coursefullname, 0, 1, 'C');
-	$pdf->Cell(0, 0, '', 0, 0, 'C');
-	$pdf->Ln();
+		// set font
+		$pdf->SetFont('times', 'B', 20);
+		// add a page
+		$pdf->AddPage();
+		$logo = '<p><img src="ualbertalogo.jpg" alt="test alt attribute" border="0" /></p>';
+		$pdf->writeHTMLCell(0, 0, '', '', $logo, 0, 1, 0, true, 'L', true);
+		$coursefullname = $course->shortname.': '.$course->fullname;
+		$pdf->Cell(0, 0, '', 0, 0, 'C');
+		$pdf->Ln();
+		$pdf->Cell(0, 0, '', 0, 0, 'C');
+		$pdf->Ln();
+		$pdf->Cell(0, 0, '', 0, 0, 'C');
+		$pdf->Ln();
+		$pdf->Cell(0, 0, '', 0, 0, 'C');
+		$pdf->Ln();
+		$pdf->Cell(0, 0, $coursefullname, 0, 1, 'C');
+		$pdf->Cell(0, 0, '', 0, 0, 'C');
+		$pdf->Ln();
 //put general course information(instructor,officehour,location) into the pdf------------------------------------------
-	$courselogistics = <<<EOD
-	Fall/Winter/Spring/Summer<br>
-	<b>Course Weight</b>: *3
+		$courselogistics = <<<EOD
+		Fall/Winter/Spring/Summer<br>
+		<b>Course Weight</b>: *3
 EOD;
-	$pdf->SetFont('times', '', 12);
-	$pdf->writeHTMLCell(0, 0, '', '', $courselogistics, 0, 1, 0, true, 'C', true);
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$courselogistics = <<<EOD
-	<b>Coordinator</b>: $courseInstructor<br>
-	<b>Office</b>: $officelocation, <b>phone</b>: $phonenumber<br>
-	<b>Email</b>: $instructoremail<br>
-	<b>Office Hours</b>: $officehours 
+		$pdf->SetFont('times', '', 12);
+		$pdf->writeHTMLCell(0, 0, '', '', $courselogistics, 0, 1, 0, true, 'C', true);
+		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+		$courselogistics = <<<EOD
+		<b>Coordinator</b>: $courseInstructor<br>
+		<b>Office</b>: $officelocation, <b>phone</b>: $phonenumber<br>
+		<b>Email</b>: $instructoremail<br>
+		<b>Office Hours</b>: $officehours 
 EOD;
-	$pdf->SetFont('times', '', 12);
-	$pdf->writeHTMLCell(0, 0, '', '', $courselogistics, 0, 1, 0, true, 'C', true);
-	$pdf->AddPage();
+		$pdf->SetFont('times', '', 12);
+		$pdf->writeHTMLCell(0, 0, '', '', $courselogistics, 0, 1, 0, true, 'C', true);
+		$pdf->AddPage();
 	
 	
 	
-//put course overview information into the pdf------------------------------------------	
-	$pdf->SetFont('times', 'B', 15);
-	$pdf->Write(5, 'Course description', '', 0, 'L', true, 0, false, false, 0);	
-	$pdf->SetFont('times', '', 10);
-	$pdf->Write(0, $coursedescription, '', 0, 'L', true, 0, false, false, 0);
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	
+//put course overview information into the pdf------------------------------------------
+		if($coursedescription != ''){
+			$decripthtml =  '<h1>Course description</h1>
+					<font size="11">'.$coursedescription.'<font>';
+			$pdf->writeHTML($decripthtml, true, false, true, false, '');
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+		}
 
 //put course reading information into the pdf------------------------------------------
-	$pdf->SetFont('times', 'B', 15);
-	$pdf->Write(5, 'Course Readings', '', 0, 'L', true, 0, false, false, 0);
-	if($readingnumber>0){
-		$pdf->SetFont('times', '', 10);
-		$readingno = 1;
-		foreach ($coursereadings as $coursereading) {
+		if($readingnumber>0){
+			$readinghtml = '<b><h1>Course Readings</h1></b><ul>';
+			foreach ($coursereadings as $coursereading) {
+				$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+				$readinghtml.= '<font size="11"><li>
+				'.$coursereading->readingname.'<br>
+				<a href="url">'.$coursereading->readingurl.'</a></li></font>';
+			}
+			$pdf->writeHTML($readinghtml, true, false, true, false, '');
 			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-			$reading = <<<EOD
-			$readingno: $coursereading->readingname
-			$coursereading->readingurl
-EOD;
-			$pdf->Write(0, $reading, '', 0, 'L', true, 0, false, false, 0);
-			$readingno ++;
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 		}
-	}
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+
 	
 	
 //put course objective information into the pdf------------------------------------------
-	$pdf->SetFont('times', 'B', 15);
-	$pdf->Write(5, 'Course Objectives', '', 0, 'L', true, 0, false, false, 0);
-	$pdf->SetFont('times', '', 10);
-	$objdes = 'The course is designed to develop the following knowledge, skills and attitudes:';
-	$pdf->Write(5, $objdes, '', 0, 'L', true, 0, false, false, 0);
-	if(isset($courseobjectives)){
-	$knowledge = <<<EOD
-		Knowledge
-		Students who successfully complete this course will be able to:
-EOD;
-	$Skills = <<<EOD
-		Skills
-		Students who successfully complete this course will be able to:
-EOD;
-	$Attitude = <<<EOD
-		Attitude
-		Students who successfully complete this course will be able to:
-EOD;
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->SetFont('times', 'B', 12);
-	$pdf->Write(10, $knowledge, '', 0, 'L', true, 0, false, false, 0);
-	$pdf->SetFont('times', '', 10);
-	foreach ($courseobjectives as $courseobjective) {
-		if($thisobj = $DB->get_record('learningobjectives', array('id'=>$courseobjective->objectiveid))){
-			if($thisobj->objectivetype == 'Knowledge'){
-				$kobj = <<<EOD
-				<ul>
-				<li>$thisobj->objectivename</li>
-				</ul>
-EOD;
-				$pdf->writeHTMLCell(0, 0, '', '', $kobj, 0, 1, 0, true, '', true);
+		if(isset($courseobjectives)){
+			$objhtml = '<b><h1>Course Objectives</h1></b>
+					<font size="11">The course is designed to develop the following knowledge, skills and attitudes:</font>';
+			
+			
+	//Knowledge learning objectives ===========================================================================================
+			$count =0;
+			$kobj = '<b><h3>Knowledge</h3><br>
+					<h4>Students who successfully complete this course will be able to:</h4></b>
+					<br><font size="11"><ul>';
+			foreach ($courseobjectives as $courseobjective) {
+				if($thisobj = $DB->get_record('learningobjectives', array('id'=>$courseobjective->objectiveid))){
+					if($thisobj->objectivetype == 'Knowledge'){
+						$count++ ;
+						$kobj.='<li>'.$thisobj->objectivename.'</li>';
+					}
+				}
 			}
-		}
-	}
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->SetFont('times', 'B', 12);
-	$pdf->Write(10, $Skills, '', 0, 'L', true, 0, false, false, 0);
-	$pdf->SetFont('times', '', 10);
-	foreach ($courseobjectives as $courseobjective) {
-		if($thisobj = $DB->get_record('learningobjectives', array('id'=>$courseobjective->objectiveid))){
-			if($thisobj->objectivetype == 'Skill'){
-				$sobj = <<<EOD
-				<ul>
-				<li>$thisobj->objectivename</li>
-				</ul>
-EOD;
-				$pdf->writeHTMLCell(0, 0, '', '', $sobj, 0, 1, 0, true, '', true);
+			if($count>0){
+				$objhtml.=$kobj;
+				$objhtml.='</ul><font><br>';
 			}
-		}
-	}
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->SetFont('times', 'B', 12);
-	$pdf->Write(10, $Attitude, '', 0, 'L', true, 0, false, false, 0);
-	$pdf->SetFont('times', '', 10);
-	foreach ($courseobjectives as $courseobjective) {
-		if($thisobj = $DB->get_record('learningobjectives', array('id'=>$courseobjective->objectiveid))){
-			if($thisobj->objectivetype == 'Attitude'){
-				$aobj = <<<EOD
-				<ul>
-				<li>$thisobj->objectivename</li>
-				</ul>
-EOD;
-				$pdf->writeHTMLCell(0, 0, '', '', $aobj, 0, 1, 0, true, '', true);
+
+	
+	//Skill learning objectives ===========================================================================================
+			$count =0;
+			$sobj = '<b><h3>Skill</h3><br>
+					<h4>Students who successfully complete this course will be able to:</h4></b>
+					<br><font size="11"><ul>';
+			foreach ($courseobjectives as $courseobjective) {
+				if($thisobj = $DB->get_record('learningobjectives', array('id'=>$courseobjective->objectiveid))){
+					if($thisobj->objectivetype == 'Skill'){
+						$count++ ;
+						$sobj.='<li>'.$thisobj->objectivename.'</li>';
+					}
+				}
 			}
+			if($count>0){
+				$objhtml.=$sobj;
+				$objhtml.='</ul><font><br>';
+			}
+
+	
+	
+	//attitude learning objectives ===========================================================================================
+			$count =0;
+			$aobj = '<b><h3>Atittude</h3><br>
+					<h4>Students who successfully complete this course will be able to:</h4></b>
+					<br><font size="11"><ul>';
+			foreach ($courseobjectives as $courseobjective) {
+				if($thisobj = $DB->get_record('learningobjectives', array('id'=>$courseobjective->objectiveid))){
+					if($thisobj->objectivetype == 'Atittude'){
+						$count++ ;
+						$aobj.='<li>'.$thisobj->objectivename.'</li>';
+					}
+				}
+			}
+			if($count>0){
+					$objhtml.=$aobj;
+					$objhtml.='</ul><font>';
+			}
+			$pdf->writeHTML($objhtml, true, false, true, false, '');
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 		}
-	}
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	}
 	
 	
 //put course grading information into the pdf------------------------------------------
-	$pdf->SetFont('times', 'B', 15);
-	$pdf->Write(5, 'Grading', '', 0, 'L', true, 0, false, false, 0);
-	$pdf->SetFont('times', '', 10);
-	$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	if (isset($courseassessments)){
-		$assessmenttable = '
-	<table border="0.1" cellspacing="0.1" cellpadding="0.1" id="gradingtable">
-    <tr>
-        <th width="17%" align="center"><b>Title</b></th>
-        <th width="10%" align="center"><b>Weight</b></th>
-        <th width="16%" align="center"><b>Date</b></th>
-		<th width="17%" align="center"><b>Type</b></th>
-		<th width="40%" align="center"><b>Description</b></th>
-    </tr>';	
-		foreach ($courseassessments as $courseassessment) {
-			$assessmenttable .= '<tr>
-					<td width="17%">'.$courseassessment->assessmentname.'</td>
-					<td width="10%" align="center">'.$courseassessment->assessmentweight.'%</td>
-					<td width="16%" align="center">'.$courseassessment->assessmentduedate.'</td>
-					<td width="17%">'.$courseassessment->type.'</td>	
-					<td width="40%">'.$courseassessment->description.'</td>	
-					</tr>';
+		if (isset($courseassessments)){
+			$assessmenthtml = '<b><h1>Grading</h1></b><br><font size="10">';
+			$assessmenthtml .= '
+		<table border="0.1" cellspacing="0.1" cellpadding="0.1" id="gradingtable">
+		<tr>
+			<th width="17%" align="center"><b>Title</b></th>
+			<th width="10%" align="center"><b>Weight</b></th>
+			<th width="16%" align="center"><b>Date</b></th>
+			<th width="17%" align="center"><b>Type</b></th>
+			<th width="40%" align="center"><b>Description</b></th>
+		</tr>';	
+			foreach ($courseassessments as $courseassessment) {
+				$assessmentduedate = $this->GetTimeStamp($courseassessment->assessmentduedate);
+				$assessmenthtml .= '<tr>
+						<td width="17%">'.$courseassessment->assessmentname.'</td>
+						<td width="10%" align="center">'.$courseassessment->assessmentweight.'%</td>
+						<td width="16%" align="center">'.$assessmentduedate.'</td>
+						<td width="17%" align="center">'.$courseassessment->type.'</td>	
+						<td width="40%">'.$courseassessment->description.'</td>	
+						</tr>';
+			}
+			$assessmenthtml .= '</table></font>';
+			$pdf->writeHTML($assessmenthtml, true, false, true, false, '');
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 		}
-		$assessmenttable .= '</table>';
-		$pdf->writeHTML($assessmenttable, true, false, true, false, '');
-		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-	}
 	
 	
 //put course session information into the pdf------------------------------------------
-		$pdf->SetFont('times', 'B', 15);
-		$pdf->Write(5, 'Course Sessions', '', 0, 'L', true, 0, false, false, 0);
 		if($sessionnumber>0){
-			$pdf->SetFont('times', '', 10);
+			$sessionhtml = '<b><h1>Course Sessions</h1></b>';
 			$sessionno = 1;
 			foreach ($coursesessions as $coursesession) {
 				$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-				$courselectures = <<<EOD
-				<font size="10"><b>$sessionno: $coursesession->sessiontitle </b></font><br><hr>
-				<b>Date</b>: $coursesession->sessiondate <br>
-				<b>Length</b>: $coursesession->sessionlength <br>
-				<b>Type</b>: $coursesession->sessiontype <br>
-				<b>Description</b>:<br> $coursesession->sessiondescription <br>
-				<b>Guest teacher</b>: $coursesession->sessionguestteacher<hr>
-EOD;
-				$pdf->writeHTMLCell(0, 0, '', '', $courselectures, 0, 1, 0, true, '', true);
+				$guestteacher = '';
+				$guestteacher = $coursesession->sessionguestteacher;
+				if($guestteacher == ''){
+					$guestteacher = 'to be assigned';
+				}
+				$sessiondate = $this->GetTimeStamp($coursesession->sessiondate);
+				$sessionhtml.= '
+				<font size="12"><b>'.$sessionno.'</b>:'. $coursesession->sessiontitle.'</font>
+				<hr>
+				<font size="11"><b>Date</b>:'. $sessiondate.' <br>
+				<b>Length</b>:'. $coursesession->sessionlength .'<br>
+				<b>Type</b>:'. $coursesession->sessiontype .'<br>
+				<b>Description</b>:<br>'. $coursesession->sessiondescription .'<br>
+				<b>Guest teacher</b>:'. $guestteacher .'<hr></font>';
 				$sessionno ++;
 			}
+			$pdf->writeHTML($sessionhtml, true, false, true, false, '');
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
+			$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 		}
-		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
-		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 	
 	
 	
 //put course policy information into the pdf------------------------------------------
-		$pdf->SetFont('times', 'B', 15);
-		$pdf->Write(5, 'Policy', '', 0, 'L', true, 0, false, false, 0);
+		$policyhtml = '<b><h1>Policy</h1></b>';
 		
+		
+		
+		$pdf->writeHTML($policyhtml, true, false, true, false, '');
 		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 		$pdf->Cell(0, 0, '', 0, 0, 'C');	$pdf->Ln();
 	
