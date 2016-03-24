4809528db3c4484341e6fa198e8ec7a30c8411b1
Diusrex <diusrex@gmail.com>
2016-03-24 11:57:59 -0600

    Improved URL setting

--- local/metadata/admview_gradatt.php
+++ local/metadata/admview_gradatt.php
@@ -21,18 +21,21 @@ $PAGE->set_title(get_string('ins_pluginname', 'local_metadata'));
 $heading = "Faculty Policy Management";
 $PAGE->set_heading($heading);
 
-// TODO: Improve how this is done
-$PAGE->set_url($CFG->wwwroot.'/local/metadata/admview_policy.php');
-$PAGE->requires->css('/local/metadata/insview_style.css');
-
 // Create url
-$base_url = create_manage_url('gradatt');
 $knowledge_url = create_manage_url('knowledge');
 $policy_url = create_manage_url('policy');
 $course_url = create_manage_url('course');
+$gradatt_url = create_manage_url('gradatt');
+$required_url = create_manage_url('required');
+
+// TODO: Improve how this is done
+$PAGE->set_url($gradatt_url);
+$PAGE->requires->css('/local/metadata/insview_style.css');
+
+
 
 // Create forms
-$gradatt_form = new gradatt_form($base_url);
+$gradatt_form = new gradatt_form($gradatt_url);
 
 
 // Submitted the data
@@ -42,7 +45,7 @@ if ($data = $gradatt_form->get_data()) {
 	} elseif (!empty($data->create_gradatt)) {
     	gradatt_form::save_data($data);
 	} 
-	redirect($base_url);
+	redirect($gradatt_url);
 } 
 
 echo $OUTPUT->header();
@@ -52,9 +55,10 @@ echo $OUTPUT->header();
 	<div class="nav_header">
 		<ul>
 		<li><a href=" <?php echo $knowledge_url; ?> ">Program Objectives</a></li>
-		<li class="onclick_nav"><a href=" <?php echo $base_url; ?> ">Graduate Attribute</a></li>
+		<li class="onclick_nav"><a href=" <?php echo $gradatt_url; ?> ">Graduate Attribute</a></li>
 		<li><a href=" <?php echo $policy_url; ?> ">Policy</a></li>
 		<li><a href=" <?php echo $course_url; ?> ">Tags</a></li>
+		<li><a href=" <?php echo $required_url; ?> ">Required</a></li>
 		</ul>
 	</div>
 	
